07c65372837ee0a363d0191f137783079804462d
Anna Akopyan <anchello@yandex.ru>
2021-06-16 15:06:29 +0300

    Добавьте файлы проекта.

---
+++ 18.cpp
@@ -0,0 +1,46 @@
+﻿// 18.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+class Stack
+{
+private:
+    int num = 0;
+    int* p_darr = new int[num];
+public:
+    void Push(int x)
+    {
+        int* p_newdarr = new int[num + 1];
+        std::cout << "Новый элемент" << x;
+        for (int i = 0; i < num; i++)
+        {
+            p_newdarr[i] = p_darr[i];
+        }
+        p_newdarr[num] = x;
+        delete[] p_darr;
+        num++;
+        p_darr = p_newdarr;
+
+    }
+    
+    int Pop(int* p_darr)
+    {
+    return p_darr[num + 1];
+    }
+    void Print()
+    {
+        for (int i = 0; i < num; i++)
+            std::cout << *(p_darr + i); 
+    }
+    Stack() 
+    {
+        delete[] p_darr;
+
+    }
+};
+int main()
+{
+    Stack v;
+    v.Print();
+}
+
